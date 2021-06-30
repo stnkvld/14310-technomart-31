952f48c706f1b6736fce73e4ff65e2d7d3a29c50
Владислав Сотников <stnkvld@gmail.com>
2021-06-30 18:56:23 +0300

    Добавил js для каталога, подправил стили хедера

--- js/script.js
+++ js/script.js
@@ -2,10 +2,6 @@
 
 const feedbackButton = document.querySelector('.button[href="#feedback"]');
 const feedbackPopup = document.querySelector('.modal.feedback');
-const feedbackForm = feedbackPopup.querySelector('.feedback__form');
-const feedbackName = feedbackForm.querySelector('[name="name"]');
-const feedbackEmail = feedbackForm.querySelector('[name="email"]');
-const feedbackText = feedbackForm.querySelector('[name="text"]');
 
 const mapButton = document.querySelector('.link[href="#map"]');
 const mapPopup = document.querySelector('.modal.map');
@@ -13,8 +9,9 @@ const mapPopup = document.querySelector('.modal.map');
 const popupCloseButtons = document.querySelectorAll('.modal__close');
 
 const specialSlider = document.querySelector('.slider');
-const specialSliderPrev = specialSlider.querySelector('.slider__btn--prev');
-const specialSliderNext = specialSlider.querySelector('.slider__btn--next');
+
+const buyButtons = document.querySelectorAll('.product-card__buy');
+const addedPopup = document.querySelector('.added-cart');
 
 let isStorageSupport = true;
 let storage = '';
@@ -34,38 +31,6 @@ function closePopup(parentPopup) {
     }, 350);
 }
 
-feedbackButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    feedbackPopup.classList.add('modal--show');
-
-    if (storage) {
-        feedbackName.value = localStorage.getItem('name');
-        feedbackEmail.value = localStorage.getItem('email');
-
-        feedbackText.focus();
-    } else {
-        feedbackName.focus();
-    }
-});
-
-feedbackForm.addEventListener('submit', function(evt) {
-    evt.preventDefault();
-
-    if (!feedbackName.value || !feedbackEmail.value) {
-        feedbackPopup.classList.add('modal--error');
-    } else if (isStorageSupport) {
-        localStorage.setItem('name', feedbackName.value);
-        localStorage.setItem('email', feedbackEmail.value);
-
-        evt.target.submit();
-    }
-});
-
-mapButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    mapPopup.classList.add('modal--show');
-});
-
 popupCloseButtons.forEach(function(item) {
     item.addEventListener('click', function(evt) {
         evt.preventDefault();
@@ -77,42 +42,100 @@ popupCloseButtons.forEach(function(item) {
 
 window.addEventListener('keydown', function(evt) {
     if (evt.key === "Esc" || evt.key === "Escape") {
-        if (feedbackPopup.classList.contains('modal--show')) {
+        if (feedbackPopup && feedbackPopup.classList.contains('modal--show')) {
             closePopup(feedbackPopup);
         }
 
-        if (mapPopup.classList.contains('modal--show')) {
+        if (mapPopup && mapPopup.classList.contains('modal--show')) {
             closePopup(mapPopup);
+        }
+
+        if (addedPopup && addedPopup.classList.contains('modal--show')) {
+            closePopup(addedPopup);
         }
     }
 });
 
-specialSliderPrev.addEventListener('click', function(evt) {
-    evt.preventDefault();
+if (feedbackPopup) {
+    const feedbackForm = feedbackPopup.querySelector('.feedback__form');
+    const feedbackName = feedbackForm.querySelector('[name="name"]');
+    const feedbackEmail = feedbackForm.querySelector('[name="email"]');
+    const feedbackText = feedbackForm.querySelector('[name="text"]');
 
-    const parentSlider = evt.target.closest('.slider');
-    const currentBullet = parentSlider.querySelector('.slider__bullet-radio:checked');
+    feedbackButton.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        feedbackPopup.classList.add('modal--show');
 
-    let prevBullet = currentBullet.previousElementSibling;
+        if (storage) {
+            feedbackName.value = localStorage.getItem('name');
+            feedbackEmail.value = localStorage.getItem('email');
 
-    currentBullet.checked = false;
-    if (!prevBullet || !prevBullet.classList.contains('slider__bullet-radio')) {
-        prevBullet = parentSlider.querySelector('.slider__bullet-radio:last-of-type');
-    }
-    prevBullet.checked = true;
-});
+            feedbackText.focus();
+        } else {
+            feedbackName.focus();
+        }
+    });
 
-specialSliderNext.addEventListener('click', function(evt) {
-    evt.preventDefault();
+    feedbackForm.addEventListener('submit', function(evt) {
+        evt.preventDefault();
 
-    const parentSlider = evt.target.closest('.slider');
-    const currentBullet = parentSlider.querySelector('.slider__bullet-radio:checked');
+        if (!feedbackName.value || !feedbackEmail.value) {
+            feedbackPopup.classList.add('modal--error');
+        } else if (isStorageSupport) {
+            localStorage.setItem('name', feedbackName.value);
+            localStorage.setItem('email', feedbackEmail.value);
 
-    let nextBullet = currentBullet.nextElementSibling;
+            evt.target.submit();
+        }
+    });
 
-    currentBullet.checked = false;
-    if (!nextBullet || !nextBullet.classList.contains('slider__bullet-radio')) {
-        nextBullet = parentSlider.querySelector('.slider__bullet-radio:first-of-type');
-    }
-    nextBullet.checked = true;
-});
+    mapButton.addEventListener('click', function(evt) {
+        evt.preventDefault();
+        mapPopup.classList.add('modal--show');
+    });
+}
+
+if (specialSlider) {
+    const specialSliderPrev = specialSlider.querySelector('.slider__btn--prev');
+    const specialSliderNext = specialSlider.querySelector('.slider__btn--next');
+
+    specialSliderPrev.addEventListener('click', function(evt) {
+        evt.preventDefault();
+
+        const parentSlider = evt.target.closest('.slider');
+        const currentBullet = parentSlider.querySelector('.slider__bullet-radio:checked');
+
+        let prevBullet = currentBullet.previousElementSibling;
+
+        currentBullet.checked = false;
+        if (!prevBullet || !prevBullet.classList.contains('slider__bullet-radio')) {
+            prevBullet = parentSlider.querySelector('.slider__bullet-radio:last-of-type');
+        }
+        prevBullet.checked = true;
+    });
+
+    specialSliderNext.addEventListener('click', function(evt) {
+        evt.preventDefault();
+
+        const parentSlider = evt.target.closest('.slider');
+        const currentBullet = parentSlider.querySelector('.slider__bullet-radio:checked');
+
+        let nextBullet = currentBullet.nextElementSibling;
+
+        currentBullet.checked = false;
+        if (!nextBullet || !nextBullet.classList.contains('slider__bullet-radio')) {
+            nextBullet = parentSlider.querySelector('.slider__bullet-radio:first-of-type');
+        }
+        nextBullet.checked = true;
+    });
+}
+
+if (addedPopup) {
+    buyButtons.forEach(function(item) {
+        item.addEventListener('click', function(evt) {
+            evt.preventDefault();
+
+            addedPopup.classList.add('modal--show');
+        });
+    });
+}
